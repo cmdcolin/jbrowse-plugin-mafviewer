f776f5fe6f142b08d76bba2222cfaabba0734d9a
Colin <colin.diesh@gmail.com>
2024-12-28 22:15:20 -0700

    Taffy

--- src/BgzipTaffyAdapter/BgzipTaffyAdapter.ts
+++ src/BgzipTaffyAdapter/BgzipTaffyAdapter.ts
@@ -1,9 +1,9 @@
 import { BaseFeatureDataAdapter } from '@jbrowse/core/data_adapters/BaseAdapter'
-import { Feature, isGzip, Region, SimpleFeature } from '@jbrowse/core/util'
+import { Feature, Region, SimpleFeature } from '@jbrowse/core/util'
 import { ObservableCreate } from '@jbrowse/core/util/rxjs'
 import { openLocation } from '@jbrowse/core/util/io'
 import { GenericFilehandle } from 'generic-filehandle2'
-import { unzip, unzipChunkSlice } from '@gmod/bgzf-filehandle'
+import { unzip } from '@gmod/bgzf-filehandle'
 import VirtualOffset from './virtualOffset'
 import Long from 'long'
 
@@ -51,10 +51,6 @@ export default class BgzipTaffyAdapter extends BaseFeatureDataAdapter {
     for (const line of text.split('\n').filter(line => line.trim())) {
       const [chr, chrStart, virtualOffset] = line.split('\t')
       const relativizedVirtualOffset = lastRawVirtualOffset + +virtualOffset
-      // console.log(
-      //   { relativizedVirtualOffset },
-      //   Number.MAX_SAFE_INTEGER / relativizedVirtualOffset,
-      // )
       const ref = chr === '*' ? lastChr : chr
       const r2 = ref.split('.').at(-1)!
       const x = Long.fromNumber(relativizedVirtualOffset)
@@ -62,6 +58,7 @@ export default class BgzipTaffyAdapter extends BaseFeatureDataAdapter {
       const z = x.and(0xffff)
       const voff = new VirtualOffset(y.toNumber(), z.toNumber())
 
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
       if (!entries[r2]) {
         entries[r2] = []
         lastChr = ''
@@ -76,16 +73,8 @@ export default class BgzipTaffyAdapter extends BaseFeatureDataAdapter {
       })
       lastChr = ref
       lastChrStart = s
-      console.log({
-        lastRawVirtualOffset,
-        relativizedVirtualOffset,
-        blockPos: relativizedVirtualOffset >>> 16,
-        lastBlockPos: lastRawVirtualOffset >>> 16,
-        x: Number.isSafeInteger(relativizedVirtualOffset),
-      })
       lastRawVirtualOffset = relativizedVirtualOffset
     }
-    console.log({ entries })
     return entries
   }
 
@@ -113,57 +102,74 @@ export default class BgzipTaffyAdapter extends BaseFeatureDataAdapter {
             }
           }
           if (firstEntry && nextEntry) {
-            console.log(
-              'fetching from: ' + firstEntry.virtualOffset.blockPosition,
-              'length: ' +
-                (nextEntry.virtualOffset.blockPosition -
-                  firstEntry.virtualOffset.blockPosition),
-              { firstEntry, nextEntry },
-            )
             const response = await file.read(
               nextEntry.virtualOffset.blockPosition -
                 firstEntry.virtualOffset.blockPosition,
               firstEntry.virtualOffset.blockPosition,
             )
-            const decoder = new TextDecoder('utf8')
-            console.log({
-              response,
-              len: response.length,
-              // @ts-expect-error
-              gzip: isGzip(response),
-            })
             const buffer = await unzip(response)
-            const data = decoder.decode(buffer)
-            console.log({ data })
-            //
-            // const text = await response.text()
-            // const lines = text.split('\n').filter(line => line.trim())
-            //
-            // for (const line of lines) {
-            //   const [chr, start, srcSize, strand, unknown, data] =
-            //     line.split('\t')
-            //
-            //   // Only emit features that overlap with query region
-            //   const featureStart = parseInt(start, 10)
-            //   const featureEnd = featureStart + parseInt(srcSize, 10)
-            //
-            //   if (
-            //     chr === query.refName &&
-            //     featureEnd >= query.start &&
-            //     featureStart <= query.end
-            //   ) {
-            //     observer.next(
-            //       new SimpleFeature({
-            //         uniqueId: `${chr}-${start}-${data}`,
-            //         refName: chr,
-            //         start: featureStart,
-            //         end: featureEnd,
-            //         strand: parseInt(strand, 10),
-            //         data: data,
-            //       }),
-            //     )
-            //   }
-            // }
+            console.log(buffer.length / 1_000_000)
+            const decoder = new TextDecoder('utf8')
+            const str = decoder.decode(
+              buffer.slice(firstEntry.virtualOffset.dataPosition),
+            )
+
+            const [firstLine, ...rest] = str.split('\n')
+            const [firstLineAlignment, info] = firstLine
+              .split(';')
+              .map(f => f.trim())
+            const ret = info.split(' ')
+            const rows = []
+            for (let i = 0; i < ret.length; i += 6) {
+              const [asm, ref] = ret[i + 2].split('.')
+              rows.push({
+                type: ret[i],
+                u0: ret[i + 1],
+                asm,
+                ref,
+                start: +ret[i + 3],
+                strand: ret[i + 4] === '-' ? -1 : 1,
+                u1: ret[i + 5],
+              })
+            }
+
+            const alignments = {} as Record<string, OrganismRecord>
+            for (const row of rows) {
+              // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+              if (!alignments[row.asm]) {
+                alignments[row.asm] = {
+                  chr: row.ref,
+                  start: row.start,
+                  srcSize: 0,
+                  strand: row.strand,
+                  unknown: 0,
+                  data: '',
+                }
+              }
+            }
+
+            const lines = [firstLineAlignment, ...rest]
+            const l = rows.length
+            const k = lines.length
+            for (let i = 0; i < l; i++) {
+              for (let j = 0; j < k; j++) {
+                alignments[rows[i].asm].data += lines[j][i]
+              }
+            }
+
+            const row0 = rows[0]
+            const aln0 = alignments[row0.asm]
+            observer.next(
+              new SimpleFeature({
+                uniqueId: `${query.refName}-${query.start}`,
+                refName: query.refName,
+                start: row0.start,
+                end: row0.start + aln0.data.length,
+                strand: row0.strand,
+                alignments,
+                seq: alignments[rows[0].asm],
+              }),
+            )
           }
         }
         observer.complete()
