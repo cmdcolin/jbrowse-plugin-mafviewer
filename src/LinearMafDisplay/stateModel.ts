4f8e2dc2d316f24b6c9f35702469d7a6a1ee3638
Colin <colin.diesh@gmail.com>
2025-02-02 18:53:18 -0500

    Lint

--- src/LinearMafDisplay/stateModel.ts
+++ src/LinearMafDisplay/stateModel.ts
@@ -213,8 +213,9 @@ export default function stateModelFactory(
     }))
     .views(self => {
       const {
+        // eslint-disable-next-line @typescript-eslint/unbound-method
         trackMenuItems: superTrackMenuItems,
-
+        // eslint-disable-next-line @typescript-eslint/unbound-method
         renderProps: superRenderProps,
       } = self
       return {
@@ -316,6 +317,7 @@ export default function stateModelFactory(
       },
     }))
     .actions(self => {
+      // eslint-disable-next-line @typescript-eslint/unbound-method
       const { renderSvg: superRenderSvg } = self
       return {
         /**
